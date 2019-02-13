67702a805b960ed87e01fb3918367e6610f68127
sangeeth <sangeeth.rajan@gmail.com>
2019-02-13 12:56:25 -0500

    Update webhook.py
    break back

--- webhook.py
+++ webhook.py
@@ -33,7 +33,7 @@ def webhook():
     for i in range(0,30):
         if date in weather[i]['dt_txt']:
             condition= weather[i]['weather'][0]['description']
-    #break
+    break
     resp = " The forecast for "+city+ "for " +date+" is " +condition
     return {
          "fulfillmentText": resp,
