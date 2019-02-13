b96bea74f6c24847a847f17b27ef392ae9b7b691
sangeeth <sangeeth.rajan@gmail.com>
2019-02-12 23:47:57 -0500

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -17,26 +17,27 @@ def webhook():
 
   res = makeResponseone(req)
   res = json.dumps(res, indent=4)
+  print(res)
   r = make_response(res)
   r.headers['Content-Type'] = 'application/json'
   return r
 
   def makeResponseone(req):
-    #result = req.get("queryResult")
-    #parameters = result.get("parameters")
-    #city= parameters.get("geo-city")
-    #date= parameters.get("date")
-    #r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=london&appid=d3720b72a53ba44d5740632909d372a1')    
-    #json_object = r.json()
-    #weather=json_object['list']
-    #for i in range(0,30):
-    #    if date in weather[i]['dt_txt']:
-            #condition= weather[i]['weather'][0]['description']
-            #break
+    result = req.get("queryResult")
+    parameters = result.get("parameters")
+    city= parameters.get("geo-city")
+    date= parameters.get("date")
+    r = requests.get('http://api.openweathermap.org/data/2.5/forecast?q=london&appid=d3720b72a53ba44d5740632909d372a1')    
+    json_object = r.json()
+    weather=json_object['list']
+    for i in range(0,30):
+        if date in weather[i]['dt_txt']:
+            condition= weather[i]['weather'][0]['description']
+            break
 
-    #resp = " The forecast for "+city+ "for " +date+" is " +condition
+    resp = " The forecast for "+city+ "for " +date+" is " +condition
 
-    resp = " The forecast for London today is sunny " +condition    
+ 
 
     return {
          "fulfillmentText": resp,
