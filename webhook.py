f8139e359142305710ba7df2242b8af0b847b7c8
sangeeth <sangeeth.rajan@gmail.com>
2019-02-12 22:04:39 -0500

    Update webhook.py
    updating queryresult

--- webhook.py
+++ webhook.py
@@ -22,7 +22,7 @@ def webhook():
   return r
 
   def makeResponseone(req):
-    result = req.get("result")
+    result = req.get("queryResult")
     parameters = result.get("parameters")
     city= parameters.get("geo-city")
     date= parameters.get("date")
